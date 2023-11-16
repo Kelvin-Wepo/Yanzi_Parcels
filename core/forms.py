b04e212006ed5dcac99e1bd6ef9d1c3461317c56
kelvin wepo <kelvinwepo7710@gmail.com>
2023-11-17 02:57:30 +0300

    ft: this commit adds forms.py

---
+++ core/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=250)
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150)
+
+    class Meta:  # It's 'Meta' not 'meta'
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        if User.objects.filter(email=email).exists():  # Fixed syntax
+            raise ValidationError("The email address already exists")
+        return email
