e1b6c50091113cd9d76f8a5894ca1697f09d98a0
kelvin wepo <kelvinwepo7710@gmail.com>
2023-11-18 04:58:28 +0300

    this commit modifies views.py

--- core/views.py
+++ core/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+
+from . import forms
+
 
 # Create your views here.
 def home(request):
    return render(request,'home.html')
 
-def customer_page(request):
-   return render(request,'home.html')
+def sign_up(request):
+   form = forms.SignUpform()
 
-def courier_page(request):
-   return render(request,'home.html')
+   if request.method == 'POST':
+     form = forms.SignUpForm(request.POST)
 
+     if form.is_valid():
+        email = form.cleaned_data.get('email').lower()
+
+        user = form.save(commit=False)
+        user.username = email
+        user.save()
+
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+        return redirect('/')
+
+
+     return render(request,'sign_up.html', {
+      'form':form
+   })
 
