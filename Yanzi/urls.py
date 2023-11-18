7183056185aff779ffc65d5a4ba7aaaf30346c05
kelvin wepo <kelvinwepo7710@gmail.com>
2023-11-18 05:07:52 +0300

    feat: this commit modifies urls.py

--- Yanzi/urls.py
+++ Yanzi/urls.py
@@ -14,14 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from core import views
+
+
+from core.customer import views as customer_views
+from core.courier import views as courier_views
+
+customer_urlpatterns = [
+    path('', customer_views.home, name="home"),
+    path('profile', customer_views.profile_page,name='profile'),
+]
+
+courier_urlpatterns =[
+    path('',courier_views.home,name="home")
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('social_django.urls', namespace='social')),
+
+
     path('',views.home),
-    path('customer/',views.customer_page),
-    path('courier/',views.courier_page)
+
+    path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html"), name='login'),
+     path('sign_out/', auth_views.LogoutView.as_view(next_page="/"), name='logout'),
+     path('sign-up/',views.sign_up),
+
+    path('customer/',include((customer_urlpatterns,'customer'))),
+    path('courier/', include ((courier_urlpatterns, 'courier'))),
    
 ]
  
