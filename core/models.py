4e7ee2d6bdf156b6c91a00d063240696b852ff6b
kelvin wepo <kelvinwepo7710@gmail.com>
2023-11-18 05:09:24 +0300

    this commit modifies models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='customer/avatar/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
