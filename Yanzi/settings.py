0cd5509ce944d72f04c5ce6a9f4bb86d91d21bf8
kelvin wepo <kelvinwepo7710@gmail.com>
2023-11-19 23:04:41 +0300

    this commit modifies settings.py

--- Yanzi/settings.py
+++ Yanzi/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    # 'core',
     'bootstrap4',
+    'social_django',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect'
             ],
         },
     },
@@ -128,3 +132,25 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL ='/sign-in/'
 LOGIN_REDIRECT_URL ='/'
+
+
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+SOCIAL_AUTH_FACEBOOK_KEY=""
+SOCAL_AUTH_FACEBOOK_SECRET=""
+SOCIAL_AUTH_FACEBOOK_SCOPE ="email"
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS ={
+    'fields':'id, name, email'
+
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS =True
+EMAIL_PORT =587
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
+DEFAULT_FROM_EMAIL ='Yanzi Parcel <no-reply@yanziparcel.localhost>'
