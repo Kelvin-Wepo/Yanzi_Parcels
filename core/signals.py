2cca9f4bc55ecd79f2bf0e39ab34137a5dd1c27d
kelvin wepo <kelvinwepo7710@gmail.com>
2023-11-21 23:25:42 +0300

    ft:this commit modifies signals.py

--- core/signals.py
+++ core/signals.py
@@ -3,23 +3,23 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.models import User
-from django. template.loader import render_to_string
+from django.template.loader import render_to_string
 
+# @receiver(post_save, sender=User)
+# def send_welcome_email(sender, instance, created, **kwargs):
+#     if created and instance.email:
+#         # send the welcome email
+#         body = render_to_string(
+#             'welcome_email_template.html',
+#             {
+#                 'name': instance.get_full_name()  # Call get_full_name as a method
+#             }
+#         )
 
-@receiver(post_save, sender=User)
-def send_welcome_email(sender, instance,created, **kwargs ):
-    if created and instance.email:
-        #send the welcome email
-        body = render_to_string
-        'welcome_email_template',
-        {
-            'name': instance.get_full_name
-        }
-
-        send_mail(
-            'Welcome to Yanzi Parcel',
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-           [instance.email],
-           fail_silently=False,
-        )
+#         send_mail(
+#             'Welcome to Yanzi Parcel',
+#             body,
+#             settings.DEFAULT_FROM_EMAIL,
+#             [instance.email],
+#             fail_silently=False,
+#         )
