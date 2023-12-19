d94bd03ec204b3a0c85f84288c6dd869bbee1b20
kelvin wepo <kelvinwepo7710@gmail.com>
2023-12-19 03:28:54 +0300

    feat:Implement the 'home' view, redirecting authenticated users to the 'available_jobs' page. The 'available_jobs' view renders the corresponding template, passing the Google Maps API key from the project settings.

--- core/courier/views.py
+++ core/courier/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from  django.conf import settings
 
-
-
-
-@login_required
+@login_required(login_url="/sign-in/?next=courier/")
 def home(request):
-   return render(request,'home.html')
+    return redirect(reverse('courier:available_jobs'))
 
 
-
+@login_required(login_url="/sign-in/?next=courier/")
+def available_jobs(request):
+    return render(request, 'courier/available_jobs.html', {
+        "GOOGLE_MAP_API_KEY":settings.GOOGLE_MAP_API_KEY
+    })
