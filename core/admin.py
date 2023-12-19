aa0aa7e803dc717f49feebf0ed99a6fd53b79e37
kelvin wepo <kelvinwepo7710@gmail.com>
2023-12-19 03:34:26 +0300

    feat: Register Courier model with Django admin

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,7 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Customer)
+admin.site.register(models.Courier)
 admin.site.register(models.Category)
 admin.site.register(models.Job)
 admin.site.register(models.Transaction)
