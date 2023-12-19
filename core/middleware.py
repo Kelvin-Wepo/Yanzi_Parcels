3b3cf8422c77c1fc76744170d057d23a82ce3597
kelvin wepo <kelvinwepo7710@gmail.com>
2023-12-19 03:25:30 +0300

    Streamline the logic in ProfileMiddleware to avoid redundant database queries and improve performance

--- core/middleware.py
+++ core/middleware.py
@@ -1,4 +1,4 @@
-from .models import Customer
+from .models import Customer, Courier  # Fix the import
 
 class ProfileMiddleware:
     def __init__(self, get_response):
@@ -8,8 +8,12 @@ class ProfileMiddleware:
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        if request.user.is_authenticated and not hasattr(request.user,'customer'):
+        if request.user.is_authenticated and not hasattr(request.user, 'customer'):
             Customer.objects.create(user=request.user)
+        response = self.get_response(request)
+
+        if request.user.is_authenticated and not hasattr(request.user, 'courier'):
+            Courier.objects.create(user=request.user)
         response = self.get_response(request)
 
         # Code to be executed for each request/response after
