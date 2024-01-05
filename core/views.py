546207d74a22059149a6648e43973d8273c36fe4
kelvin wepo <kelvinwepo7710@gmail.com>
2024-01-05 14:29:05 +0300

    this commit modifies views.py

--- core/views.py
+++ core/views.py
@@ -1,22 +1,21 @@
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-
+from .forms import SignUpForm
+from django.utils.timezone import make_aware
 
 
 from . import forms
-
-
-# Create your views here.
 def home(request):
-   return render(request,'home.html')
-
+    current_datetime = datetime.now()
+    return render(request, 'home.html', {'current_datetime': current_datetime})
 
 def sign_up(request):
-    form = forms.SignUpForm()  # Corrected the capitalization of SignUpForm
+    form = SignUpForm()
 
     if request.method == 'POST':
-        form = forms.SignUpForm(request.POST)
+        form = SignUpForm(request.POST)
 
         if form.is_valid():
             email = form.cleaned_data.get('email').lower()
